e891d135ee0789e32b05dce04c4174362f569995
scrrpaw47 <lzudeme47@gmail.com>
2025-03-14 07:57:47 +0100

    1

---
+++ BlogWelcomeScript.js
@@ -0,0 +1,9 @@
+// Dieses Skript dient momentan nur als Platzhalter.
+// Hier kannst du später dynamische Inhalte oder interaktive Funktionen ergänzen.
+
+console.log("Blog-Website geladen!");
+
+// Beispiel: Begrüßungs-Popup (oder Info) beim Laden der Seite
+window.addEventListener("load", () => {
+  alert("Willkommen auf meinem Blog!");
+});
